1ffc416b818f1e757eb8134da3b67f00809affde
Maria Eduarda Batista de Oliveira <100208704+projectduetec@users.noreply.github.com>
2023-12-05 19:54:16 -0300

    Add files via upload

---
+++ Exercicio_18_04/Banco.cs
@@ -0,0 +1,47 @@
+﻿namespace Exercicio_18_04;
+class Banco
+{   
+
+    public int Numero {get; set;}
+    private double Saldo { get;  set;}
+    public double Limite {get; private set;}
+
+    public void AjustarLimite(double valor){
+        this.Limite = valor;
+    }
+    public void Depositar( double valor){
+        this.Saldo += valor;
+    }
+    public void Sacar(double valor){
+        if (this.Saldo + this.Limite <= valor){
+            Console.WriteLine("Você está sacando mais do que possui!");
+        }else{
+             this.Saldo -= valor;
+        }
+    }
+    public double MostrarSaldo(){
+        return this.Saldo + this.Limite;
+    }
+
+
+    public double parcela {get; set;}
+    public double valor {get; set;}
+
+    public string emprestimo(int parcelas, double dinheiro){
+        this.parcela = parcelas;
+        this.valor = dinheiro;
+        
+        double divi = dinheiro/parcelas;
+        double multi = dinheiro * 0.06;
+        double soma = multi + divi;
+        double total = soma * parcelas;
+
+        string resultado = "O valor da sua parcela sem juros é: "+ divi +". O valor da parcela com juros: "+ multi + ". O valor do juros com prestações é de: "+ soma +"e o valor total a pagar é de: "+total;
+        return resultado;
+        
+     
+    }
+    public void imprimi(string texto){
+        Console.WriteLine(texto);
+    }
+}
---
+++ Exercicio_18_04/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio_18_04;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto Banco DANIEL");
+        Banco b = new Banco();
+      
+        b.Depositar(1000);
+        b.Sacar(1600);
+       
+        b.AjustarLimite(500);
+
+        double valortotal = b.MostrarSaldo();
+
+       
+
+        Console.WriteLine("O seu saldo total é de " + valortotal);
+        
+
+        b.imprimi(b.emprestimo(12,1000));
+    }
+}
