28df638f3d8efca8eae3e65fb24de2071c954f6e
Maria Eduarda Batista de Oliveira <100208704+projectduetec@users.noreply.github.com>
2023-12-05 20:01:44 -0300

    Add files via upload

---
+++ projeto_conta/Conta.cs
@@ -0,0 +1,26 @@
+﻿namespace projeto_conta;
+class Conta
+{
+  public int Numero {get; set;}
+  private double Saldo {get; set;}
+  public double Limite{get; private set;}
+  public void AjustarLimite(double valor){
+    this.Limite = valor;
+  }
+  public void Depositar (double valor){
+    this.Saldo  += valor;
+ }
+   public void Sacar (double valor){
+    if (valor <- this.Saldo + this.Limite){
+       this.Saldo  -= valor;
+    }else{
+        Console.WriteLine("Você não possui saldo+Limite suficiente para esse Saque!");
+        Console.WriteLine("Saque não realizado!");
+    }
+    
+  }
+
+  public double MostrarSaldo(){
+    return this.Saldo + this.Limite;
+  }
+}
---
+++ projeto_conta/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace projeto_conta;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto banco!");
+        Conta c = new Conta();  //instanciar uma classe
+        //c.Saldo = 1000; 
+        c.Depositar(1000);  
+        c.Sacar (83000);        //atribuir valor
+        //c.Limite = 500;
+        c.AjustarLimite(500);
+        double ValorTotal = c.MostrarSaldo();
+
+
+       // Console.WriteLine("Seu saldo é de: " + c.Saldo + " Com limite de: " + c.Limite);
+        Console.WriteLine("Seu saldo total é de: " + ValorTotal);
+        //Console.WriteLine("Seu saldo total é de: " + c.Saldo);
+    }
+}
